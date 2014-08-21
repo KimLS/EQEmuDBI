32bd28d9366628b9dbdc8eca26e841edd47f9c63
KimLS <KLS@peqtgc.com>
2014-08-21 03:16:58 -0700

    Fixes for __VA_ARGS__

--- test-sqlite/main.cpp
+++ test-sqlite/main.cpp
@@ -23,20 +23,20 @@
 #include "../dbi/sth.h"
 #include "../dbi/rs.h"
 
-#define PrintErr(x, ...) printf("Error at line %d: " x, __LINE__, __VA_ARGS__)
+#define PrintErr(x, ...) printf("Error at line %d: " x "\n", __LINE__, ##__VA_ARGS__)
 
 int main() {
 	DBI::DatabaseAttributes attr;
 	
 	auto dbh = DBI::DatabaseInterface::Instance()->Connect("sqlite", "test.db", "", "", "", attr);
 	if(!dbh) {
-		PrintErr("Failure to connect.\n");
-		return 0;
+		PrintErr("Failure to connect.");
+		return 1;
 	}
 
 	auto rs = dbh->Do("DROP TABLE IF EXISTS db_test");
 	if(!rs) {
-		PrintErr("Failure to drop table.\n");
+		PrintErr("Failure to drop table.");
 	}
 
 	rs = dbh->Do("CREATE TABLE db_test ("
@@ -47,18 +47,18 @@ int main() {
 		"blob_value BLOB)");
 
 	if(!rs) {
-		PrintErr("Failure to get result from create table. Error: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from create table. Error: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	if(!dbh->Ping()) {
-		PrintErr("Failed to ping.\n");
-		return 0;
+		PrintErr("Failed to ping.");
+		return 1;
 	}
 
 	if(!dbh->Begin()) {
-		PrintErr("Failed to begin.\n");
-		return 0;
+		PrintErr("Failed to begin.");
+		return 1;
 	}
 
 	rs = dbh->Do("INSERT INTO db_test (id, int_value, real_value, text_value, blob_value) VALUES(?, ?, ?, ?, ?)",
@@ -70,12 +70,12 @@ int main() {
 
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to insert value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to insert value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from insert: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from insert: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	std::string blob_value;
@@ -89,12 +89,12 @@ int main() {
 
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to insert value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to insert value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from insert: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from insert: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = dbh->Do("INSERT INTO db_test (id, int_value, real_value, text_value, blob_value) VALUES(?, ?, ?, ?, ?)",
@@ -106,12 +106,12 @@ int main() {
 
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to insert value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to insert value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from insert: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from insert: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = dbh->Do("INSERT INTO db_test (id, int_value, real_value, text_value, blob_value) VALUES(?, ?, ?, ?, ?)",
@@ -123,12 +123,12 @@ int main() {
 
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to insert value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to insert value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from insert: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from insert: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = dbh->Do("INSERT INTO db_test (id, int_value, real_value, text_value, blob_value) VALUES(?, ?, ?, ?, ?)",
@@ -140,312 +140,312 @@ int main() {
 
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to insert value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to insert value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from insert: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from insert: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	if(!dbh->Commit()) {
-		PrintErr("Failed to commit.\n");
-		return 0;
+		PrintErr("Failed to commit.");
+		return 1;
 	}
 
 	auto sth = dbh->Prepare("INSERT INTO db_test (id, int_value, real_value, text_value, blob_value) VALUES(?, ?, ?, ?, ?)");
 	if(!sth) {
-		PrintErr("Failed to create prepared statement.\n");
-		return 0;
+		PrintErr("Failed to create prepared statement.");
+		return 1;
 	}
 
 	rs = sth->Execute(6, 2134, 125.9, std::string("Test value"), blob_value);
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to insert value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to insert value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from insert: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from insert: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	sth = dbh->Prepare("SELECT int_value, real_value, text_value, blob_value FROM db_test WHERE id = ?");
 	if(!sth) {
-		PrintErr("Failed to create prepared statement.\n");
-		return 0;
+		PrintErr("Failed to create prepared statement.");
+		return 1;
 	}
 
 	rs = sth->Execute(1);
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to select value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to select value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 
 		auto row = (*rs->Rows().begin());
 		if(!row["int_value"].is_null) {
-			PrintErr("Row int_value was incorrect value in row 1\n");
-			return 0;
+			PrintErr("Row int_value was incorrect value in row 1");
+			return 1;
 		}
 
 		if(!row["real_value"].is_null) {
-			PrintErr("Row real_value was incorrect value in row 1\n");
-			return 0;
+			PrintErr("Row real_value was incorrect value in row 1");
+			return 1;
 		}
 
 		if(!row["text_value"].is_null) {
-			PrintErr("Row text_value was incorrect value in row 1\n");
-			return 0;
+			PrintErr("Row text_value was incorrect value in row 1");
+			return 1;
 		}
 
 		if(!row["blob_value"].is_null) {
-			PrintErr("Row blob_value was incorrect value in row 1\n");
-			return 0;
+			PrintErr("Row blob_value was incorrect value in row 1");
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from select: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from select: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = sth->Execute(2);
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to select value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to select value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 
 		auto row = (*rs->Rows().begin());
 		if(row["int_value"].is_null) {
-			PrintErr("Row int_value was incorrect value in row 2\n");
-			return 0;
+			PrintErr("Row int_value was incorrect value in row 2");
+			return 1;
 		} else {
 			uint64_t val = std::stoll(row["int_value"].value);
 			if(val != 5) {
-				PrintErr("Row int_value was incorrect value in row 2\n");
-				return 0;
+				PrintErr("Row int_value was incorrect value in row 2");
+				return 1;
 			}
 		}
 
 		if(row["real_value"].is_null) {
-			PrintErr("Row real_value was incorrect value in row 2\n");
-			return 0;
+			PrintErr("Row real_value was incorrect value in row 2");
+			return 1;
 		} else {
 			double val = std::stod(row["real_value"].value);
 			if(val > 126.0 || val < 125.8) {
-				PrintErr("Row real_value was incorrect value in row 2\n");
-				return 0;
+				PrintErr("Row real_value was incorrect value in row 2");
+				return 1;
 			}
 		}
 
 		if(row["text_value"].is_null) {
-			PrintErr("Row text_value was incorrect value in row 2\n");
-			return 0;
+			PrintErr("Row text_value was incorrect value in row 2");
+			return 1;
 		} else {
 			if(row["text_value"].value.compare("A test value") != 0) {
-				PrintErr("Row text_value was incorrect value in row 2\n");
-				return 0;
+				PrintErr("Row text_value was incorrect value in row 2");
+				return 1;
 			}
 		}
 
 		if(row["blob_value"].is_null) {
-			PrintErr("Row blob_value was incorrect value in row 2\n");
-			return 0;
+			PrintErr("Row blob_value was incorrect value in row 2");
+			return 1;
 		} else {
 			if(row["blob_value"].value.length() != 12) {
-				PrintErr("Row blob_value was incorrect length in row 2\n");
-				return 0;
+				PrintErr("Row blob_value was incorrect length in row 2");
+				return 1;
 			}
 
 			if(memcmp(row["blob_value"].value.c_str(), "hello\0world\0", 12) != 0) {
-				PrintErr("Row blob_value was incorrect value in row 2\n");
-				return 0;
+				PrintErr("Row blob_value was incorrect value in row 2");
+				return 1;
 			}
 		}
 	} else {
-		PrintErr("Failure to get result from select: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from select: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = sth->Execute(3);
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to select value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to select value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 
 		auto row = (*rs->Rows().begin());
 		if(row["int_value"].is_null) {
-			PrintErr("Row int_value was incorrect value in row 3\n");
-			return 0;
+			PrintErr("Row int_value was incorrect value in row 3");
+			return 1;
 		} else {
 			uint64_t val = std::stoll(row["int_value"].value);
 			if(val != 556) {
-				PrintErr("Row int_value was incorrect value in row 3\n");
-				return 0;
+				PrintErr("Row int_value was incorrect value in row 3");
+				return 1;
 			}
 		}
 
 		if(row["real_value"].is_null) {
-			PrintErr("Row real_value was incorrect value in row 3\n");
-			return 0;
+			PrintErr("Row real_value was incorrect value in row 3");
+			return 1;
 		} else {
 			double val = std::stod(row["real_value"].value);
 			if(val > 126.0 || val < 125.8) {
-				PrintErr("Row real_value was incorrect value in row 3\n");
-				return 0;
+				PrintErr("Row real_value was incorrect value in row 3");
+				return 1;
 			}
 		}
 
 		if(!row["text_value"].is_null) {
-			PrintErr("Row text_value was incorrect value in row 3\n");
-			return 0;
+			PrintErr("Row text_value was incorrect value in row 3");
+			return 1;
 		}
 
 		if(!row["blob_value"].is_null) {
-			PrintErr("Row blob_value was incorrect value in row 3\n");
-			return 0;
+			PrintErr("Row blob_value was incorrect value in row 3");
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from select: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from select: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = sth->Execute(4);
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to select value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to select value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 
 		auto row = (*rs->Rows().begin());
 		if(row["int_value"].is_null) {
-			PrintErr("Row int_value was incorrect value in row 4\n");
-			return 0;
+			PrintErr("Row int_value was incorrect value in row 4");
+			return 1;
 		} else {
 			uint64_t val = std::stoll(row["int_value"].value);
 			if(val != 518012) {
-				PrintErr("Row int_value was incorrect value in row 4\n");
-				return 0;
+				PrintErr("Row int_value was incorrect value in row 4");
+				return 1;
 			}
 		}
 
 		if(!row["real_value"].is_null) {
-			PrintErr("Row real_value was incorrect value in row 4\n");
-			return 0;
+			PrintErr("Row real_value was incorrect value in row 4");
+			return 1;
 		}
 
 		if(!row["text_value"].is_null) {
-			PrintErr("Row text_value was incorrect value in row 4\n");
-			return 0;
+			PrintErr("Row text_value was incorrect value in row 4");
+			return 1;
 		}
 
 		if(!row["blob_value"].is_null) {
-			PrintErr("Row blob_value was incorrect value in row 4\n");
-			return 0;
+			PrintErr("Row blob_value was incorrect value in row 4");
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from select: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from select: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = sth->Execute(5);
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to select value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to select value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 
 		auto row = (*rs->Rows().begin());
 		if(row["int_value"].is_null) {
-			PrintErr("Row int_value was incorrect value in row 5\n");
-			return 0;
+			PrintErr("Row int_value was incorrect value in row 5");
+			return 1;
 		} else {
 			uint64_t val = std::stoll(row["int_value"].value);
 			if(val != 42949672960) {
-				PrintErr("Row int_value was incorrect value in row 5\n");
-				return 0;
+				PrintErr("Row int_value was incorrect value in row 5");
+				return 1;
 			}
 		}
 
 		if(!row["real_value"].is_null) {
-			PrintErr("Row real_value was incorrect value in row 5\n");
-			return 0;
+			PrintErr("Row real_value was incorrect value in row 5");
+			return 1;
 		}
 
 		if(!row["text_value"].is_null) {
-			PrintErr("Row text_value was incorrect value in row 5\n");
-			return 0;
+			PrintErr("Row text_value was incorrect value in row 5");
+			return 1;
 		}
 
 		if(!row["blob_value"].is_null) {
-			PrintErr("Row blob_value was incorrect value in row 5\n");
-			return 0;
+			PrintErr("Row blob_value was incorrect value in row 5");
+			return 1;
 		}
 	} else {
-		PrintErr("Failure to get result from select: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from select: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
 	rs = sth->Execute(6);
 	if(rs) {
 		if(rs->AffectedRows() != 1) {
-			PrintErr("Failure to select value: %s\n", dbh->ErrorMessage().c_str());
-			return 0;
+			PrintErr("Failure to select value: %s", dbh->ErrorMessage().c_str());
+			return 1;
 		}
 
 		auto row = (*rs->Rows().begin());
 		if(row["int_value"].is_null) {
-			PrintErr("Row int_value was incorrect value in row 6\n");
-			return 0;
+			PrintErr("Row int_value was incorrect value in row 6");
+			return 1;
 		} else {
 			uint64_t val = std::stoll(row["int_value"].value);
 			if(val != 2134) {
-				PrintErr("Row int_value was incorrect value in row 6\n");
-				return 0;
+				PrintErr("Row int_value was incorrect value in row 6");
+				return 1;
 			}
 		}
 
 		if(row["real_value"].is_null) {
-			PrintErr("Row real_value was incorrect value in row 6\n");
-			return 0;
+			PrintErr("Row real_value was incorrect value in row 6");
+			return 1;
 		} else {
 			double val = std::stod(row["real_value"].value);
 			if(val > 126.0 || val < 125.8) {
-				PrintErr("Row real_value was incorrect value in row 6\n");
-				return 0;
+				PrintErr("Row real_value was incorrect value in row 6");
+				return 1;
 			}
 		}
 
 		if(row["text_value"].is_null) {
-			PrintErr("Row text_value was incorrect value in row 6\n");
-			return 0;
+			PrintErr("Row text_value was incorrect value in row 6");
+			return 1;
 		} else {
 			if(row["text_value"].value.compare("Test value") != 0) {
-				PrintErr("Row text_value was incorrect value in row 6\n");
-				return 0;
+				PrintErr("Row text_value was incorrect value in row 6");
+				return 1;
 			}
 		}
 
 		if(row["blob_value"].is_null) {
-			PrintErr("Row blob_value was incorrect value in row 6\n");
-			return 0;
+			PrintErr("Row blob_value was incorrect value in row 6");
+			return 1;
 		} else {
 			if(row["blob_value"].value.length() != 12) {
-				PrintErr("Row blob_value was incorrect length in row 6\n");
-				return 0;
+				PrintErr("Row blob_value was incorrect length in row 6");
+				return 1;
 			}
 
 			if(memcmp(row["blob_value"].value.c_str(), "hello\0world\0", 12) != 0) {
-				PrintErr("Row blob_value was incorrect value in row 6\n");
-				return 0;
+				PrintErr("Row blob_value was incorrect value in row 6");
+				return 1;
 			}
 		}
 	} else {
-		PrintErr("Failure to get result from select: %s\n", dbh->ErrorMessage().c_str());
-		return 0;
+		PrintErr("Failure to get result from select: %s", dbh->ErrorMessage().c_str());
+		return 1;
 	}
 
-	printf("Tests passed\n");
+	printf("Tests passed");
 	return 0;
 }
